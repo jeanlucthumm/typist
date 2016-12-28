15de2a4bbe2860854d74b2dbf0f14ae9edd4bc11
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2016-12-27 16:59:45 -0800

    Write to file

--- main.cpp
+++ main.cpp
@@ -43,6 +43,49 @@ void two_sequence() {
     // Sort frequencies
     sort(table.begin(), table.end(), greater_pair);
 
+    // Output to file
+    ofstream out{"two-seq-freq.csv", ios::out};
+    for (auto &elem : table) {
+        cout << elem.first << " : " << elem.second << endl; // DEBUG
+        out << elem.first << "," << elem.second << endl;
+    }
+
+    infile.close();
+    out.close();
+}
+
+// Most common three letter combinations
+void three_sequence() {
+    unsigned long counter[26][26] = {0};
+    vector<pair<string, unsigned long>> table; // holds <freq, string>
+    ifstream infile("w.csv");   // use csvparser.py to generate this
+
+    // Process every word
+    string word;
+    while (getline(infile, word)) {
+        // Find letter combinations
+        for (int i = 0; i < word.size() - 1; i++) {
+            int let1 = word[i] - 'a';
+            int let2 = word[i + 1] - 'a';
+            counter[let1][let2]++; // register combination
+        }
+    }
+
+    // Get a sortable list of the frequencies
+    for (int i = 0; i < 26; i++) {
+        for (int j = 0; j < 26; j++) {
+            if (counter[i][j] != 0) {
+                string s = "";
+                s += (char) (i + 'a');
+                s += (char) (j + 'a');
+                table.push_back(pair<string, unsigned long>{s, counter[i][j]});
+            }
+        }
+    }
+
+    // Sort frequencies
+    sort(table.begin(), table.end(), greater_pair);
+
     // DEBUG
     for (auto &elem : table) {
         cout << elem.first << " : " << elem.second << endl;
